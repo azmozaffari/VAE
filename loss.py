75306bd10f7d309c39c537f9724f6176cd1065d0
Azadeh Mozafari <40042087+azmozaffari@users.noreply.github.com>
2023-11-21 15:47:16 +0000

    loss function is updated

--- loss.py
+++ loss.py
@@ -2,17 +2,8 @@ import torch
 import torch.nn as nn
 
 def loss_function(recon_x, x, mu, std):
+    
     reconstruction_function = nn.MSELoss(size_average=False)
     BCE = reconstruction_function(recon_x, x)  # mse loss
-    
-    # loss = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    
-    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
-    KLD = torch.sum(KLD_element).mul_(-0.5)
-    
-    # KL divergence
-    
+    KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
     return BCE + KLD
-
-
-def 
