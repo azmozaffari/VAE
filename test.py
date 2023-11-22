59dc72134238c14e2f334cd23cf3bf8c09276bf2
Azadeh Mozafari <40042087+azmozaffari@users.noreply.github.com>
2023-11-22 23:29:33 +0000

    test.py is updated

--- test.py
+++ test.py
@@ -11,9 +11,22 @@ model.eval()
 # genereate normal gaussian noise
 eps = torch.normal(0, 1, size=(10, 20))
 
+grid = torch.zeros(1,20)
+grid[0,19] = 0.05
+grid[0,10] = 0.05
+grid[0,1] = 0.05
+
+ln = torch.zeros(64,20) + 0.2
+
+for i in range(64):
+    ln[i,:] = ln[i,:] + i*grid
+
+
+
+
 
 # call the decoder to generate samples from the normal disribution
-rec_img = model.decoder(eps)
+rec_img = model.decoder(ln)
 rec_img = rec_img.view(-1,28,28)
 rec_img = torch.unsqueeze(rec_img, 1)
 
