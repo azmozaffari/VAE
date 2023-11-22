c12640d18063019a969eae2ae588cba73ab57b7e
Azadeh Mozafari <40042087+azmozaffari@users.noreply.github.com>
2023-11-22 17:16:43 +0000

    train is updated

--- train.py
+++ train.py
@@ -15,77 +15,91 @@ from loss import *
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(" the code is running on: ", device)
-# SEED = 42
+SEED = 42
 
-# def deterministic(seed):
-#     """
-#     Setup execution state so that we can reproduce multiple executions.
-#     Make the execution "as deterministic" as possible.
+def deterministic(seed):
+    """
+    Setup execution state so that we can reproduce multiple executions.
+    Make the execution "as deterministic" as possible.
 
-#     random_seed: seed used to feed torch, numpy and python random
-#     """
-#     random.seed(seed)
-#     torch.manual_seed(seed)
-#     if torch.cuda.is_available():
-#         torch.backends.cudnn.deterministic = True
-#         torch.cuda.manual_seed_all(seed)      
+    random_seed: seed used to feed torch, numpy and python random
+    """
+    random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = True
+        torch.cuda.manual_seed_all(seed)      
        
     
-# deterministic(SEED)
+deterministic(SEED)
 
 
 
-# lr = 1e-3 
-# batch_size = 16
-# model = VAE(28,28,1024,500)
+lr = 1e-3 
+batch_size = 16
+model = VAE(28,28,1024,500)
 
-# model = model.to(device)
-# epochs = 100
+model = model.to(device)
+epochs = 100
 
 
-# optimizer = optim.Adam(model.parameters(), lr)
+optimizer = optim.Adam(model.parameters(), lr)
 
-# img_transform = transforms.Compose([
-#     transforms.ToTensor()
-#     ])
+img_transform = transforms.Compose([
+    transforms.ToTensor()
+    ])
 
-# dataset = MNIST('./data', transform=img_transform, train = True, download=True)
-# dataloader = DataLoader(dataset, batch_size = batch_size, shuffle=True)
+dataset = MNIST('./data', transform=img_transform, train = True, download=True)
+dataloader = DataLoader(dataset, batch_size = batch_size, shuffle=True)
 
-# def train(epochs,  model,dataloader,optimizer):
+def train(epochs,  model,dataloader,optimizer):
     
     
     
     
-#     for epoch in range(epochs):
-#         for batch_id, data in enumerate(dataloader):
-#             img, label = data
-#             img = img.to(device)
+    for epoch in range(epochs):
+        total_loss = 0
+        for batch_id, data in enumerate(dataloader):
+            img, label = data
+            img = img.to(device)
 
 
           
             
-#             r_img, mu,log_var = model(img)
-#             img = img.squeeze()
-#             row = img.size(1)
-#             col = img.size(2)
+            r_img, mu,log_var = model(img)
+            img = img.squeeze()
+            row = img.size(1)
+            col = img.size(2)
             
 
             
-#             loss = loss_function(r_img, img.view(-1,row*col), mu, log_var)
+            loss = loss_function(r_img, img.view(-1,row*col), mu, log_var)
 
-#             optimizer.zero_grad()
-#             loss.backward()
-#             optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-#             if epoch%10 == 0:
-#                 print("epoch:", epoch,  "loss is", loss.item())
+            # track the loss is dropping
+            # save the model in models folder
+            # how to expand this model for the RGB images like CIFAR
+            
+            total_loss += loss.item() 
+
+            
+        n_samples = batch_id* batch_size
+        print("epoch:", epoch,  "loss is", total_loss/n_samples)
+    
+        if epoch%50 == 0:
+            
+            torch.save(model.state_dict(), './models/vae_model'+str(epoch)+'.pth')
+
+            
 
             
         
 
 
 
-# train(epochs,  model, dataloader, optimizer)
+train(epochs,  model, dataloader, optimizer)
 
 
